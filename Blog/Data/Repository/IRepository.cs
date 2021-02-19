ab32526eadddc97eda3581f5a150eda5f2b6c4be
Jordan <jrashkov@gmail.com>
2021-02-19 12:43:57 +0200

    Add Repository
    Create interface with crud methods

---
+++ Blog/Data/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Repository
+{
+    public interface IRepository
+    {
+        Post GetPost(int id);
+        List<Post> GetAllPosts(int id);
+        bool AddPost(Post post);
+        bool RemovePost(int id);
+        bool UpdatePost(Post post);
+    }
+}
